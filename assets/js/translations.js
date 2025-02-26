25ec174e2aad3dfba876b2f13b3271e58749e338
Israel Cruz <israel.crz@outlook.com>
2025-02-25 22:45:29 -0600

    Adding reCaptcha

---
+++ assets/js/translations.js
@@ -0,0 +1,73 @@
+{
+    "en": {
+      "company_name": "ProServeOne: Cleaning Services",
+      "location": "Laredo TX, USA",
+      "email": "servicehub@proserveone.com",
+      "phone": "+1 (956) 523-1987 or +1 (956) 471-2083",
+      
+      "nav_home": "Home",
+      "nav_services": "Services",
+      "nav_about": "About us",
+      "nav_contact": "Contact now",
+      
+      "hero_subtitle": "Cleaning Services",
+      "hero_description": "A clean office is a productive office. At ProServe One, we deliver top-tier cleaning services designed to meet the unique needs of your business. From daily maintenance to deep cleaning, our professional team ensures every corner of your workspace shines. Experience the difference of a truly spotless environment with ProServe One.",
+      "services_btn": "Our Services", 
+      "contact_btn": "Contact now",
+      
+      "services_title": "Services",
+      "cleaning_services_trust": "Cleaning Services You Can Trust",
+      "cleaning_services_description": "We provide professional cleaning services tailored to your needs, ensuring a spotless and healthy environment for your business.",
+      "office_cleaning": "Office Cleaning",
+      "office_cleaning_description": "Keep your workspace spotless and productive with our comprehensive cleaning services. We ensure every corner of your office shines.",
+      "deep_cleaning": "Deep Cleaning Services",
+      "deep_cleaning_description": "Go beyond surface cleaning with our intensive deep cleaning services. Perfect for seasonal refreshes or post-construction cleanup.",
+      "restroom_cleaning": "Restroom Cleaning and Sanitization",
+      "restroom_cleaning_description": "Ensure a hygienic and pleasant restroom experience for employees and visitors.",
+      "carpet_cleaning": "Carpet and Upholstery Cleaning",
+      "carpet_cleaning_description": "Revitalize your office’s carpets and furniture with our professional cleaning services.",
+      "general_maintenance": "General Maintenance",
+      "general_maintenance_description": "Electrical, plumbing, welding, carpentry and solutions for storage facilities.",
+      
+      "request_quote": "Request a Quote",
+      "request_quote_description": "Interested in our cleaning services? Request a personalized quote and discover how we can meet your specific needs.",
+      "contact_us": "Contact us",
+
+    },
+    "es": {
+      "company_name": "ProServeOne: Servicios de Limpieza",
+      "location": "Laredo TX, EE.UU.",
+      "email": "servicehub@proserveone.com",
+      "phone": "+1 (956) 523-1987 ó +1 (956) 471-2083",
+      
+      "nav_home": "Inicio",
+      "nav_services": "Servicios",
+      "nav_about": "Sobre nosotros",
+      "nav_contact": "Contáctanos",
+      
+      "hero_subtitle": "Servicios de Limpieza",
+      "hero_description": "Una oficina limpia es una oficina productiva. En ProServe One, ofrecemos servicios de limpieza de primer nivel diseñados para satisfacer las necesidades únicas de tu negocio. Desde mantenimiento diario hasta limpieza profunda, nuestro equipo profesional asegura que cada rincón de tu espacio de trabajo brille. Experimenta la diferencia de un entorno realmente impecable con ProServe One.",
+      "services_btn": "Nuestros servicios", 
+      "contact_btn": "Contáctanos",
+      
+      "services_title": "Servicios",
+      "cleaning_services_trust": "Servicios de limpieza en los que puedes confiar",
+      "cleaning_services_description": "Ofrecemos servicios de limpieza profesional adaptados a tus necesidades, asegurando un ambiente impecable y saludable para tu negocio.",
+      "office_cleaning": "Limpieza de oficinas",
+      "office_cleaning_description": "Mantén tu espacio de trabajo impecable y productivo con nuestros servicios de limpieza integral. Nos aseguramos de que cada rincón de tu oficina brille.",
+      "deep_cleaning": "Servicios de limpieza profunda",
+      "deep_cleaning_description": "Ve más allá de la limpieza superficial con nuestros servicios de limpieza profunda. Perfecto para renovaciones estacionales o limpieza post-construcción.",
+      "restroom_cleaning": "Limpieza y desinfección de baños",
+      "restroom_cleaning_description": "Garantiza una experiencia higiénica y agradable en los baños para empleados y visitantes.",
+      "carpet_cleaning": "Limpieza de alfombras y tapicería",
+      "carpet_cleaning_description": "Revitaliza las alfombras y muebles de tu oficina con nuestros servicios de limpieza profesional.",
+      "general_maintenance": "Mantenimiento general",
+      "general_maintenance_description": "Electricidad, plomería, soldadura, carpintería y soluciones para almacenes.",
+    
+      "request_quote": "Solicita una Cotización",
+      "request_quote_description": "¿Interesado en nuestros servicios de limpieza? Solicita una cotización personalizada y descubre cómo podemos satisfacer tus necesidades específicas.",
+      "contact_us": "Contáctanos",
+
+    }
+  }
+  
--- send_email.php
+++ send_email.php
@@ -14,48 +14,70 @@ require 'PHPMailer/PHPMailer.php';
 require 'PHPMailer/SMTP.php';
 require 'PHPMailer/Exception.php';
 
-$mail = new PHPMailer(true);
+if (isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])) {
 
-try {
-    // SMTP setup
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com'; 
-    $mail->SMTPAuth = true;
-    $mail->Username = 'servicehub@proserveone.com'; 
-    $mail->Password = 'lsdk qngr rvat wogw'; 
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port = 587;
+    $recaptcha = $_POST['g-recaptcha-response'];
 
-    // Form data
-    $firstName = htmlspecialchars($_POST['formFirstName']);
-    $lastName = htmlspecialchars($_POST['formLastName']);
-    $email = htmlspecialchars($_POST['formEmail']);
-    $phone = htmlspecialchars($_POST['formPhone']);
-    $message = nl2br(htmlspecialchars($_POST['formMessages']));
+    $secret_key = '6LdS7uIqAAAAAJXgZRcuRmlURVkP2T2DImP6iFHX';
+
+    $url = 'https://www.google.com/recaptcha/api/siteverify?secret='.$secret_key.'&response='.$recaptcha;
+
+    $response = file_get_contents($url);
+
+    $response = json_decode($response);
+
+    if ($response->success) {
+        $mail = new PHPMailer(true);
+
+        try {
+            // SMTP setup
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com'; 
+            $mail->SMTPAuth = true;
+            $mail->Username = 'servicehub@proserveone.com'; 
+            $mail->Password = 'lsdk qngr rvat wogw'; 
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $mail->Port = 587;
+
+            // Form data
+            $firstName = htmlspecialchars($_POST['formFirstName']);
+            $lastName = htmlspecialchars($_POST['formLastName']);
+            $email = htmlspecialchars($_POST['formEmail']);
+            $phone = htmlspecialchars($_POST['formPhone']);
+            $message = nl2br(htmlspecialchars($_POST['formMessages']));
 
 
-    $mail->setFrom($email, "$firstName $lastName");
-    $mail->addAddress('servicehub@proserveone.com');
+            $mail->setFrom($email, "$firstName $lastName");
+            $mail->addAddress('servicehub@proserveone.com');
 
-    // email
-    $mail->isHTML(true);
-    $mail->Subject = "New message from $firstName $lastName";
-    $mail->Body = "<h1>ProServe One - New message!</h1>
-                   <p><strong>Name:</strong> $firstName $lastName</p>
-                   <p><strong>Email:</strong> $email</p>
-                   <p><strong>Phone:</strong> $phone</p>
-                   <p><strong>Message:</strong><br>$message</p>";
+            // email
+            $mail->isHTML(true);
+            $mail->Subject = "New message from $firstName $lastName";
+            $mail->Body = "<h1>ProServe One - New message!</h1>
+                        <p><strong>Name:</strong> $firstName $lastName</p>
+                        <p><strong>Email:</strong> $email</p>
+                        <p><strong>Phone:</strong> $phone</p>
+                        <p><strong>Message:</strong><br>$message</p>";
 
-    // $mail->SMTPDebug = 2; 
-    // $mail->Debugoutput = 'html'; 
+            // $mail->SMTPDebug = 2; 
+            // $mail->Debugoutput = 'html';
 
-    // send email
-    if ($mail->send()) {
-        echo "Message sent successfully.";
+            
+
+            // send email
+            if ($mail->send()) {
+                echo "Message sent successfully.";
+            } else {
+                echo "Error: " . $mail->ErrorInfo;
+            }
+        } catch (Exception $e) {
+            echo "Error: " . $mail->ErrorInfo;
+        }
     } else {
-        echo "Error: " . $mail->ErrorInfo;
+        $_SESSION['status'] = "Error in recaptcha verification";
+        header("Location: {$_SERVER["HTTP_REFERER"]}");
+        exit(0);
     }
-} catch (Exception $e) {
-    echo "Error: " . $mail->ErrorInfo;
 }
+
 ?>
